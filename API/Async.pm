7000e5ea16bc69031cb13d6c284223951c825a26
smoothquark <ramesh@rr7.com>
2025-03-15 22:14:18 +0000

    Allow _filterAlbums to show albums according to whether DASH and Atmos are enabled

--- API/Async.pm
+++ API/Async.pm
@@ -157,7 +157,7 @@ sub _filterAlbums {
 
 	my $explicitAlbumHandling = $prefs->get('explicitAlbumHandling') || {};
 
-	my (%seen, %explicit, %nonExplicit);
+	my (%seen, %explicit, %nonExplicit, %include);
 	my $wantsExplicit = $explicitAlbumHandling->{$self->userId} || 0;
 	my $wantsNonExplicit = !$wantsExplicit || $explicitAlbumHandling->{$self->userId} == 2;
 	my $wantsBoth = $wantsExplicit && $wantsNonExplicit;
@@ -175,6 +175,7 @@ sub _filterAlbums {
 		scalar (grep /^LOSSLESS$/ || ($prefs->get('enableAtmos') eq '1' ? /^DOLBY_ATMOS$/ : /^$/), @{$_->{mediaMetadata}->{tags} || []})
 			&& $explicitFilter->($_->{explicit}, $fingerprint)
 			&& !$seen{$fingerprint}++
+			&& $include{$fingerprint} eq '1'
 	} map {
 		my $item = $_;
 		my $item_tag = Plugins::TIDAL::API::getMediaInfo($item)->{media_tag};
@@ -182,6 +183,17 @@ sub _filterAlbums {
 
 		$explicit{$fingerprint} ||= $_->{explicit};
 		$nonExplicit{$fingerprint} ||= !$_->{explicit};
+
+		# check item_tag to see if album will be included based on its quality
+		if ( ($prefs->get('enableAtmos') eq '1') && ($item_tag eq '[A]')) {
+			$include{$fingerprint} = '1';
+		}
+		elsif ( ($prefs->get('enableDASH') eq '1') && ($item_tag eq '[M]')) {
+			$include{$fingerprint} = '1';
+		}
+		elsif ( $item_tag eq '[H]') {
+			$include{$fingerprint} = '1';	# include LOSSLESS [High] by default
+		}
 
 		$item->{fingerprint} = $fingerprint;
 		$item;
